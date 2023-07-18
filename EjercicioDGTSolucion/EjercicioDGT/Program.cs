f20870930ac8810bffa4ba873d8edec4109b0936
pmpcurso1 <pmpcurso1@PMPW1280>
2023-07-18 12:45:11 +0200

    Agregar archivos de proyecto.

---
+++ EjercicioDGTSolucion/EjercicioDGT/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EjercicioDGT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehiculo1 = new Vehiculo(1000, new DateTime(2015, 01, 01), Vehiculo.EtiquetaDGT.Sin);
+            var vehiculo2 = new Vehiculo(1000, new DateTime(2018, 01, 01), Vehiculo.EtiquetaDGT.C);
+            var vehiculo3 = new Vehiculo(1000, new DateTime(2020, 01, 01), Vehiculo.EtiquetaDGT.CERO);
+
+            var lista = new List<Vehiculo>();
+            lista.Add(vehiculo1);
+            lista.Add(vehiculo2);
+            lista.Add(vehiculo3);
+
+            foreach (var vehiculo in lista) 
+            {
+                var impuesto = vehiculo.CalcularImpuestoCirculacion();
+                Console.WriteLine($"El impuesto del vehiculo es {impuesto} euros");
+            }
+        }
+    }
+}
---
+++ EjercicioDGTSolucion/EjercicioDGT/Vehiculo.cs
@@ -0,0 +1,64 @@
+﻿namespace EjercicioDGT
+{
+    public class Vehiculo
+    {
+        public enum EtiquetaDGT{
+            Sin,
+            B,
+            C,
+            ECO,
+            CERO
+        }
+
+        public Vehiculo()
+        {
+            
+        }
+
+        public Vehiculo(double impuestoBase, DateTime fechaMat, EtiquetaDGT etiqueta)
+        {
+            ImpuestoBase = impuestoBase;
+            FechaMat = fechaMat;
+            Etiqueta = etiqueta;
+        }
+
+        public double ImpuestoBase { get; set; }
+
+        public DateTime FechaMat { get; set; }
+
+        public EtiquetaDGT Etiqueta { get; set; }
+
+        public double CalcularImpuestoCirculacion()
+        {
+            var antiguedad = DateTime.Now.Year - FechaMat.Year;
+            var impuesto = ImpuestoBase + (ImpuestoBase * 0.01 * antiguedad);
+
+            switch (Etiqueta)
+            {
+                case EtiquetaDGT.Sin:
+                    impuesto += ImpuestoBase * 0.25;
+                    break;
+
+                case EtiquetaDGT.B:
+                    impuesto += ImpuestoBase * 0.15;
+                    break;
+
+                case EtiquetaDGT.C:
+                    impuesto += ImpuestoBase * 0.10;
+                    break;
+
+                case EtiquetaDGT.ECO:
+                    impuesto += ImpuestoBase * 0.05;
+                    break;
+
+                case EtiquetaDGT.CERO:
+                    // No suma nada
+                    break;
+
+                default:
+                    break;
+            }
+            return impuesto;
+        }
+    }
+}
