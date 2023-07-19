d2076b4cc08d732fec5e1136d9bba3d36f52d5c8
FriquiLuquin <friquiluquin@gmail.com>
2023-07-19 10:11:25 +0200

    Modulo 6 Ejercicio 1

---
+++ Modulo6/ConstructoresVehiculos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Intrinsics;
+
+namespace ConstructoresVehiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construir vehiculo con fecha de compra y color
+            var v1 = new Vehiculo(new DateTime(2020, 01, 01), "Blanco");
+
+            // Construir vehiculo con marca y modelo
+            var v2 = new Vehiculo("Tesla", "X");
+
+            // Construir vehiculo con fecha de compra, color, marca y modelo
+            var v3 = new Vehiculo(new DateTime(2020, 01, 01), "Gris", "Dacia", "Duster");
+
+            var lista = new List<Vehiculo>();
+            lista.Add(v1);
+            lista.Add(v2);
+            lista.Add(v3);
+
+            foreach (var v in lista)
+            {
+                if (!string.IsNullOrEmpty(v.Marca) && !string.IsNullOrEmpty(v.Modelo))
+                {
+                    Console.WriteLine($"El vehiculo es un {v.Marca} {v.Modelo}");
+                }
+                if (!string.IsNullOrEmpty(v.Color)) { Console.WriteLine($"El vehiculo es de color {v.Color}"); }
+                if (v.FechaCompra != DateTime.MinValue) { Console.WriteLine($"El vehiculo se compro en {v.FechaCompra}"); }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Modulo6/ConstructoresVehiculos/Vehiculo.cs
@@ -0,0 +1,36 @@
+﻿namespace ConstructoresVehiculos
+{
+    internal class Vehiculo
+    {
+        public Vehiculo(DateTime fechaCompra, string color)
+        {
+            FechaCompra = fechaCompra;
+            Color = color;
+
+        }
+
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+
+        }
+
+        public Vehiculo(DateTime fechaCompra, string color, string marca, string modelo)
+        {
+            FechaCompra = fechaCompra;
+            Color = color;
+            Marca = marca;
+            Modelo = modelo;
+
+        }
+
+        public DateTime FechaCompra { get; set; }
+
+        public string Color { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+    }
+}
