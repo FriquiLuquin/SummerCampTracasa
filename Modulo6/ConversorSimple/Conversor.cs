cbe97f9dbcfaf46f63b298d4a5ca50b884cecc28
FriquiLuquin <friquiluquin@gmail.com>
2023-07-19 12:59:58 +0200

    Modulo 6 Ejercicio 2

---
+++ Modulo6/ConversorSimple/Conversor.cs
@@ -0,0 +1,51 @@
+﻿namespace ConversorSimple
+{
+    internal class Conversor
+    {
+        public enum Divisas
+        {
+            Euros,
+            Dolares,
+            Libras
+        }
+
+        const double FactorEurosADolares = 1.12;
+        const double FactorEurosALibras = 0.87;
+        const double FactorDolaresAEuros = 0.89;
+        const double FactorDolaresALibras = 0.77;
+        const double FactorLibrasAEuros = 1.15;
+        const double FactorLibrasADolares = 1.29;
+
+        public static double ConvertirEntreDivisas(Divisas divisaOrigen, Divisas divisaDestino, double cantidad)
+        {
+            var resultado = 0d;
+
+            switch ((divisaOrigen, divisaDestino))
+            {
+                case (Divisas.Euros, Divisas.Dolares):
+                    resultado = cantidad * FactorEurosADolares;
+                    break;
+                case (Divisas.Euros, Divisas.Libras):
+                    resultado = cantidad * FactorEurosALibras;
+                    break;
+                case (Divisas.Dolares, Divisas.Euros):
+                    resultado = cantidad * FactorDolaresAEuros;
+                    break;
+                case (Divisas.Dolares, Divisas.Libras):
+                    resultado = cantidad * FactorDolaresALibras;
+                    break;
+                case (Divisas.Libras, Divisas.Euros):
+                    resultado = cantidad * FactorLibrasAEuros;
+                    break;
+                case (Divisas.Libras, Divisas.Dolares):
+                    resultado = cantidad * FactorLibrasADolares;
+                    break;
+                default:
+                    resultado = cantidad;
+                    break;
+            }
+
+            return Math.Round(resultado, 2);
+        }
+    }
+}
---
+++ Modulo6/ConversorSimple/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConversorSimple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringDivisaOrigen = string.Empty;
+            var stringDivisaDestino = string.Empty;
+            var stringCantidad = string.Empty;
+            var stringExit = string.Empty;
+            bool exit = false;
+
+            Console.WriteLine("Bienvenido al conversor de divisas.");
+
+            do
+            {
+                Console.WriteLine($"- {Conversor.Divisas.Euros}");
+                Console.WriteLine($"- {Conversor.Divisas.Dolares}");
+                Console.WriteLine($"- {Conversor.Divisas.Libras}");
+
+                Console.Write("Introduce la divisa de origen: ");
+                stringDivisaOrigen = Console.ReadLine();
+                Enum.TryParse(stringDivisaOrigen, out Conversor.Divisas divisaOrigen);
+
+                Console.Write("Introduce la divisa de destino: ");
+                stringDivisaDestino = Console.ReadLine();
+                Enum.TryParse(stringDivisaDestino, out Conversor.Divisas divisaDestino);
+
+                Console.Write("Introduce la cantidad a convertir: ");
+                stringCantidad = Console.ReadLine();
+                Double.TryParse(stringCantidad, out var cantidad);
+
+                var resultado = Conversor.ConvertirEntreDivisas(divisaOrigen, divisaDestino, cantidad);
+                Console.WriteLine($"{cantidad} {divisaOrigen} equivalen a {resultado} {divisaDestino}");
+
+                Console.WriteLine();
+                Console.Write("¿Quieres hacer otra conversion? (y/n): ");
+                stringExit = Console.ReadLine();
+                if (!string.IsNullOrEmpty(stringExit) && !(stringExit.ToLower().StartsWith('y')))
+                {
+                    exit = true;
+                }
+
+            } while (!exit);
+
+        }
+    }
+}
