7ed6a911938eb48fc2125f3c18896d6667e663c6
FriquiLuquin <friquiluquin@gmail.com>
2023-07-26 14:12:55 +0200

    (De)serializar JSON

--- Ficheros/02TextFileLines/DataProcessor/JsonFileProcessor.cs
+++ Ficheros/02TextFileLines/DataProcessor/JsonFileProcessor.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DataProcessor
 {
@@ -16,16 +19,21 @@ namespace DataProcessor
         public void Process()
         {
             // Using read all text
-            //string originalText = File.ReadAllText(InputFilePath);
-            //string processedText = originalText.ToUpperInvariant();
-            //File.WriteAllText(OutputFilePath, processedText);
-
-            // Using read all lines
-            // string[] lines = File.ReadAllLines(InputFilePath);
-            // lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file
-            // File.WriteAllLines(OutputFilePath, lines);
+            string originalText = File.ReadAllText(InputFilePath);
 
             // TODO: Procesar los ficheros JSON
+
+            List<Moneda> listaMonedas = JsonConvert.DeserializeObject<List<Moneda>>(originalText);
+
+            foreach(Moneda m in listaMonedas)
+            {
+                Console.WriteLine($"{m.nombre}, {m.codigo}, {m.valorEnDolares}");
+                m.nombre = m.nombre.ToUpperInvariant();
+            }
+
+            string processedText = JsonConvert.SerializeObject(listaMonedas);
+            // string[] lines = processedText.Split(new char[] {'' });
+            File.WriteAllText(OutputFilePath, processedText);
         }
     }
 }
