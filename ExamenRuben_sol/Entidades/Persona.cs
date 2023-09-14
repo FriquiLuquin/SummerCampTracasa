fc8dc032a1016998cc31a1b7695721524f724429
FriquiLuquin <friquiluquin@gmail.com>
2023-09-14 08:28:34 +0200

    cambios

---
+++ ExamenRuben_sol/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        
+        public DateTime FechaNacimiento { get; set; }
+         [MaxLength(25)]
+        public string? Telefono { get; set; }
+    }
+}
